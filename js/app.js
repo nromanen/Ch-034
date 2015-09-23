c312f9105a0af456cbe47462e564a9f1cb4e976e
vitya11 <Ofir.cv@gmail.com>
2015-09-23 22:38:55 +0300

    add jQuery UI css + img + Datepicker(demo)

--- js/app.js
+++ js/app.js
@@ -2,3 +2,11 @@ $('[data-toggle="popover"]').popover({container: 'body'})
 $('form').on("click", function(e){
 	e.preventDefault();
 })
+  //Datepicker
+$(function() {
+    $( "#datepicker" ).datepicker({
+      changeMonth: true,
+      changeYear: true,
+      autoSize: true
+    });
+  });
