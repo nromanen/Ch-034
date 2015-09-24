ad87940c671366c4921f0752b00e60f450e4d273
Sergii Diak <buispr@gmail.com>
2015-09-25 02:37:50 +0300

    refactored validation error placement

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 $(function() {
+    //Popover
     $('[data-toggle="popover"]').popover({container: 'body'});
-    $('form').on("click", function(e){
-       e.preventDefault();
-    });
 
     // Datepicker
     $( "#datepicker" ).datepicker({
@@ -11,68 +9,75 @@ $(function() {
         yearRange: "1930:2015"
     });
 
+    //Phone number mask
+    $("input[name='phonenumber']").mask("(999) 999-99-99");
     jQuery.validator.addMethod("edult", function(value, element) {
         var currentDate = new Date();
         var userAge = new Date(Date.parse(value))
         return this.optional(element) || ((currentDate.getFullYear() - userAge.getFullYear() ) > 18);
     }, "You so young!!!");
 
-
     // localization
     $(".lang").click(function(){
         $(".lang").toggle();
     });
+    //Form validation
+    $("#regform").validate({
+        
+        focusCleanup: true,
+        onkeyup: false,
 
-    // phone-mask
-    $("input[name='phonenumber']").mask("(999) 999-99-99");
-});
-
-$("#regform").validate({
-
-        errorElement : 'div',
-        errorClass: 'error',
-        onfocusout : function(element) {
-            $(element).valid();
-        },
-        onfocusin : function(element) {
+        submitHandler: function(form) {
             
-            //$(".error").remove();
         },
-        rules : {
-            firstname : {
+
+        invalidHandler: function(event, validator) {
+            validator.valid();
+        },
+
+        rules: {
+            login: {
+                required: true,
+                minlength: 5,
+                maxlength: 40,
+                pattern: /^[A-Za-z0-9\._\-]*$/
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            firstname: {
                 required : true,
                 minlength : 3,
                 maxlength : 30,
                 pattern : /^[A-ZА-ЯЄІЇ][\sA-ZА-ЯЄІЇa-zа-яєії'0-9]*$/
             },
-            phonenumber : {
-                required : true
-            },
-            birthdate : {
-                required : true,
-                edult    : true
-            }
         },
-        messages : {
-            firstname : {
-                required : $('enter your name').html(),
-                minlength : $('productNameTooShort').html(),
-                maxlength : $('productNameTooLong').html(),
-                pattern : $('productNameIllegalCharacters').html()
+
+        messages: {
+            firstname: {
+                required: $('enter your name').html(),
+                minlength: $('productNameTooShort').html(),
+                maxlength: $('productNameTooLong').html(),
+                pattern: $('productNameIllegalCharacters').html()
             },
             birthdate : {
                  required : $('I need date').html(),
                  edult    : $('WTF').html()
             }
         },
-        errorPlacement : function(error, element) {
-            console.log(error);
-            $('#firstnameglyph').attr("title", error.toString);
-            error.appendTo(element.closest('div'));  console.log(error.toString);
-        }
-    });
 
-$('.btn').click(
-            function() {
-                $("#regform").valid();
-            });
+        errorPlacement: function(error, element) {
+            error.appendTo(element.closest('div'));
+        },
+
+        highlight: function(element) {
+            $(element).closest('.form-group').addClass('has-error');
+        },
+
+        unhighlight: function (element) {
+            $(element).closest('.form-group').removeClass('has-error');
+        }
+
+    });
+});
