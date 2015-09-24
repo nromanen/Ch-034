ab8d0b526a07c035a7010ff3ee1c19704bf2c933
Diak Sergii <buispr@gmail.com>
2015-09-24 18:27:07 +0300

    Cleaned app.js code for better readability

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 $(function() {
-	$('[data-toggle="popover"]').popover({container: 'body'})
+	$('[data-toggle="popover"]').popover({container: 'body'});
 	$('form').on("click", function(e){
 		e.preventDefault();
-	})
-	
-  //Datepicker
-    $( "#datepicker" ).datepicker({
-      changeMonth: true,
-      changeYear: true,
-      yearRange: "1930:2015"
-    });
-  }); 
+	});
+
+	//Datepicker
+	$( "#datepicker" ).datepicker({
+		changeMonth: true,
+		changeYear: true,
+		yearRange: "1930:2015"
+	});
+});
