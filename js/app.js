b6bc915f5572eccdca9b75ad577bc98738eb5e78
Sviatoslav <sv.fedechko@gmail.com>
2015-09-24 19:05:21 +0300

    first name validation

--- js/app.js
+++ js/app.js
@@ -11,3 +11,42 @@ $(function() {
         yearRange: "1930:2015"
     });
 });
+
+$("#regform").validate({
+
+        errorElement : 'div',
+        errorClass: 'error',
+        onfocusout : function(element) {
+            $(element).valid();
+        },
+        onfocusin : function(element) {
+            
+            // $(".error").remove();
+        },
+        rules : {
+            firstname : {
+                required : true,
+                minlength : 3,
+                maxlength : 30,
+                pattern : /^[A-ZА-ЯЄІЇ][\sA-ZА-ЯЄІЇa-zа-яєії'0-9]*$/
+            }
+        },
+        messages : {
+            firstname : {
+                required : $('enter your name').html(),
+                minlength : $('productNameTooShort').html(),
+                maxlength : $('productNameTooLong').html(),
+                pattern : $('productNameIllegalCharacters').html()
+            }
+        },
+        errorPlacement : function(error, element) {
+            console.log(error);
+            $('#firstnameglyph').attr("title", error.toString);
+            error.appendTo(element.closest('div'));
+        }
+    });
+
+$('.btn').click(
+            function() {
+                $("#regform").valid();
+            });
