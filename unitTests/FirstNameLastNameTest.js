4df6ebe3910b0454629147aacfc33e48c38cc8f8
Sviatoslav <sv.fedechko@gmail.com>
2015-09-30 14:25:20 +0300

    firstName, lastName tests

--- unitTests/FirstNameLastNameTest.js
+++ unitTests/FirstNameLastNameTest.js
@@ -1,4 +1,4 @@
-  function some(){
-        // var some = true;
-        return true;
-    }
+ function testName(testName) {
+	var regexp = /^[А-ЯЄІЇ]'?[а-яєії']*[а-яєії]*\-[А-ЯЄІЇ][а-яєії']+$|^[А-ЯЄІЇ][а-яєії']+$/;
+	return regexp.test(testName);
+};
