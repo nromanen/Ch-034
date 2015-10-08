8e2a12539c9c629de112c713abc2809037a2fe6b
vitya11 <Ofir.cv@gmail.com>
2015-10-08 11:40:36 +0300

    test2

--- Gruntfile.js
+++ Gruntfile.js
@@ -151,5 +151,4 @@ module.exports = function(grunt ) {
     grunt.registerTask('server:dev', ['connect:dev', 'watch']);
     grunt.registerTask('server:prod', ['connect:prod', 'watch']);
     grunt.registerTask('default', []);
-    //test
 };
