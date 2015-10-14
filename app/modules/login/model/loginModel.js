129b992ee113c335cdc4206b3106f615948b3f75
vmehera123 <v.mehera123@gmail.com>
2015-10-14 15:27:05 +0300

    add validation

--- app/modules/login/model/loginModel.js
+++ app/modules/login/model/loginModel.js
@@ -3,9 +3,17 @@ define(function(require){
 
     var CMS = require("CMS"),
 	    Model = CMS.Model.extend({
+	       
 	        defaults: {
 	            email: null,
 	            password: null
+	        },
+
+	        validate: function(attr, options) {
+	        	
+	        	if ( !attr.email ) {
+	        		return "Помилка! заповніть підсвічені поля та спробуйте знову.";
+	        	} 
 	        }
 	    });
 
--- app/modules/login/view/loginView.js
+++ app/modules/login/view/loginView.js
@@ -8,13 +8,37 @@ define(function(require) {
 
 			initialize: function() {
 				this.render();
+				this.$el.find( ".error-message" ).hide();
+				this.listenTo( this.model, "invalid", this.errorMessage() );
 			},
 
-			template: _.template( require("text!../template/loginTemplate.html") ),
+			template: _.template( require( "text!../template/loginTemplate.html" ) ),
 
 			render: function() {
 				this.$el.html( this.template( this.model.toJSON() ) );
 				return this;
+			},
+
+			events: {
+				"blur #email" : "validEmail",
+				"blur #password" : "validPassword"
+			},
+
+			validEmail: function() {
+				var newEmail = $( "#email" ).val();
+				this.$el.find( ".email-input" ).removeClass( "has-error" );
+				this.$el.find( ".error-message" ).hide();
+				this.model.set( { email: newEmail }, { validate: true } );
+			},
+
+			validPassword: function() {
+				//var form = $( "#loginForm" ).serializeObject();
+				//this.model.set( form, { validate: true } );
+			},
+
+			errorMessage: function() {
+				this.$el.find( ".email-input" ).addClass( "has-error" ); 
+				this.$el.find( ".error-message" ).show();
 			}
 		});
 
