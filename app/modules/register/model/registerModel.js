a416f176ca53604f368d64d97bb6caa0a24c5480
vitya11 <vtarntc@chclass11.training.local>
2015-10-16 18:39:39 +0300

    register markup update

--- app/modules/register/model/registerModel.js
+++ app/modules/register/model/registerModel.js
@@ -12,51 +12,8 @@ define(function(require){
 	            repeatPass : null
 	        }
 
-            validate: function(attrs) {
-                var me = this;
-                var options = {
-                    success: function () {
-                        me.hideErrors();
-                    },
-                    error: function (model, errors) {
-                        me.showErrors(errors);
-                    }
-                };
-
-                showErrors: function(errors) {
-                    _.each(errors, function (error) {
-                        var controlGroup = this.$('.' + error.name);
-                        controlGroup.addClass('error');
-                        controlGroup.find('.help-inline').text(error.message);
-                    }, this);
-                },
-
-                hideErrors: function () {
-                    this.$('.control-group').removeClass('error');
-                    this.$('.help-inline').text('');
-                }
-
-                var errors = [];
-
-                if(!attrs.name) {
-                    errors.push({name: 'name', message: 'Please, enter your name.'});
-                }
-                if(!attrs.surname) {
-                    errors.push({name: 'surname', message: 'Please, enter your surname.'});
-                }
-                if(!attrs.email) {
-                    errors.push({name: 'email', message: 'Please, fill email field.'});
-                }
-                if(!attrs.pass) {
-                    errors.push({name: 'pass', message: 'Please, enter your password.'});
-                }
-                if(!attrs.email) {
-                    errors.push({name: 'repeatPass', message: 'Please, enter your password.'});
-                }
-
-                return errors.length > 0 ? errors : false;
-
-            }
+           
+           
 	    });
 
     return Model;
