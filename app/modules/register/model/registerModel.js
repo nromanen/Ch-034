f5c263a18a1572f7f47cb92c633c16e3fa690c69
vitya11 <vtarntc@chclass11.training.local>
2015-10-20 19:37:43 +0300

    start to do validation

--- app/modules/register/model/registerModel.js
+++ app/modules/register/model/registerModel.js
@@ -10,10 +10,33 @@ define(function(require){
 	            email      : null,
 	            pass       : null,
 	            repeatPass : null
-	        }
+	        },
 
-           
-           
+	        validate: function(attr, options) {
+	        	var errors = [];
+
+	        	if ( !attr.name ) {
+	        		errors.push( { name: "name" } );
+	        	}
+
+	        	if ( !attr.surname ) {
+	        		errors.push( { name: "surname" } );
+	        	}
+	        	if ( !attr.email ) {
+	        		errors.push( { name: "email" } );
+	        	}
+
+	        	if ( !attr.pass ) {
+	        		errors.push( { name: "pass" } );
+	        	}
+	        	
+	        	if ( !attr.repeatPass ) {
+	        		errors.push( { name: "repeatPass" } );
+	        	}
+	        	
+	        				
+	        	return errors.length > 0 ? errors : false;
+	        }
 	    });
 
     return Model;
