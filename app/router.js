8beff1ef71493302a8745b9cc8eeb1977b06019a
Diak Sergiy <buispr@gmail.com>
2015-10-21 18:14:22 +0300

    implemented fix of pegeable collection to module_courses

--- app/router.js
+++ app/router.js
@@ -63,7 +63,7 @@ define(function(require) {
 
 
         showRegisterModule: function(){
-            this.module = new RegisterModule.View( {model: this.register} )
+            this.module = new RegisterModule.View( {model: this.register} );
         },    
 
         showTestModule: function(courseModule, moduleTest, currentQuestion){    
