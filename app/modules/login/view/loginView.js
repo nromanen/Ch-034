e1ed154ba290a8cc7c1a13e1985c58dc14a17a56
vmehera123 <v.mehera123@gmail.com>
2015-10-21 09:57:58 +0300

    fix

--- app/modules/login/view/loginView.js
+++ app/modules/login/view/loginView.js
@@ -43,7 +43,7 @@ define( function ( require ) {
 
 				this.model.set( dataObj , { validate: true } );
 			},
-				
+
 			errorMessage: function ( model, errors ) {
 				_.forEach( errors, function ( error ) {
 					this.$el.find( "." + error ).addClass( "error" ); 
