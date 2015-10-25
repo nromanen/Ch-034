bbc684a47ef3fd3adb626531a0de76227878d456
Mila <merzavko.oi@gmail.com>
2015-10-25 14:50:02 +0200

    add 2 header's menu

---
+++ app/modules/core/views/headerNavigationMenuView.js
@@ -0,0 +1,13 @@
+define(function(require) {
+   "use strict";
+
+   var View = require("../view"),
+
+       HeaderNavigationMenuView = View.extend({
+        template: _.template(require("text!../templates/headerNavigationMenuTemplate.html")),
+
+        el: false
+   });
+
+   return HeaderNavigationMenuView;
+});
---
+++ app/modules/core/views/headerPersonalMenuView.js
@@ -0,0 +1,13 @@
+define(function(require) {
+    "use strict";
+
+    var View = require("../view"),
+
+        HeaderPersonalMenuView = View.extend({
+            template: _.template(require("text!../templates/headerPersonalMenuTemplate.html")),
+
+            el: false
+        });
+
+    return HeaderPersonalMenuView;
+});
