db5e375b8a7fab9eed0abdc2075ae93e26759c33
unknown <abiliatc@chclass09.training.local>
2015-10-27 18:52:33 +0200

    Email validation.

--- app/modules/register/model/registerModel.js
+++ app/modules/register/model/registerModel.js
@@ -2,36 +2,40 @@ define(function(require){
     "use strict";
 
     var CMS = require("CMS"),
+    function isEmail(email) {
+        var emailRegex = "/^[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~-]+(\.[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~-]+)*@([a-zA-Z0-9]([a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+([a-zA-Z]{2,4}|museum|travel)$/";
+        return emailRegex.test(email);
+    }
         Model = CMS.Model.extend({
             
             defaults: {
-	            name       : null,
-	            surname    : null,
-	            email      : null,
-	            pass       : null,
-	            repeatPass : null
-	        },
+                name       : null,
+                surname    : null,
+                email      : null,
+                pass       : null,
+                repeatPass : null
+            },
 
             validate: function(attr, options) {
                 var errors = [];
 
-                if ( !attr.name ) {
-                    errors.push( "name" );
+                if (!attr.name) {
+                    errors.push("name");
                 }
 
-                if ( !attr.surname ) {
-                    errors.push( "surname" );
+                if (!attr.surname) {
+                    errors.push("surname");
                 }
-                if ( !attr.email ) {
-                    errors.push( "email" );
+                if (!(isEmail(attr.email))) {
+                    errors.push("email");
                 }
 
-                if ( !attr.pass ) {
-                    errors.push( "pass" );
+                if (!attr.pass) {
+                    errors.push("pass");
                 }
                 
                 if ( !attr.repeatPass ) {
-                    errors.push( "repeatPass" );
+                    errors.push("repeatPass");
                 }
                      
                 return errors.length > 0 ? errors : false;
