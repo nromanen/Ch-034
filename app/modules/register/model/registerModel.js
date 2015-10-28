ab8a378b5cea9ae8edd0eef2b96a7b5c13f89857
vitya11 <vtarntc@chclass11.training.local>
2015-10-28 16:55:02 +0200

    some fixes according remarks

--- app/modules/register/model/registerModel.js
+++ app/modules/register/model/registerModel.js
@@ -3,40 +3,32 @@ define(function(require){
 
     var CMS = require("CMS"),
         Model = CMS.Model.extend({
-            
             defaults: {
-	            name       : null,
-	            surname    : null,
-	            email      : null,
-	            pass       : null,
-	            repeatPass : null
-	        },
-
+            name       : null,
+            surname    : null,
+            email      : null,
+            pass       : null,
+            repeatPass : null
+        },
             validate: function(attr, options) {
                 var errors = [];
-
                 if ( !attr.name ) {
                     errors.push( "name" );
                 }
-
                 if ( !attr.surname ) {
                     errors.push( "surname" );
                 }
                 if ( !attr.email ) {
                     errors.push( "email" );
                 }
-
                 if ( !attr.pass ) {
                     errors.push( "pass" );
                 }
-                
                 if ( !attr.repeatPass ) {
                     errors.push( "repeatPass" );
                 }
-                     
                 return errors.length > 0 ? errors : false;
             }
         });
-
     return Model;
 });
