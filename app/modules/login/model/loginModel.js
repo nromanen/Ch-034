14a687cbcffad3d188571832c2e5a71e79c4b3fa
vmehera123 <v.mehera123@gmail.com>
2015-10-28 13:07:25 +0200

    general improvement

--- app/modules/login/model/loginModel.js
+++ app/modules/login/model/loginModel.js
@@ -1,29 +1,29 @@
 define(function(require){
-    "use strict";
+   "use strict";
 
-    var CMS = require("CMS"),
-	    Model = CMS.Model.extend({
-	       
-	        defaults: {
-	            email: null,
-	            password: null
-	        },
+  var CMS = require("CMS"),
+	  Model = CMS.Model.extend({
+	  	
+	    defaults: {
+	      email: null,
+	      password: null
+	    },
 
-	        validate: function(attr, options) {
+	    validate: function(attr, options) {
 	        	
-	        	var errors = [];
+	      var errors = [];
 
-	        	if ( !attr.email) {
-	        		errors.push( ".email" );
-	        	}
+	      if ( !attr.email) {
+	        errors.push( ".email" );
+	      }
 
-	        	if ( !attr.password ) {
-	        		errors.push( ".password" );
-	        	}
-	        	
-	        	return errors.length > 0 ? errors : false;
-	        }
-	    });
+	      if ( !attr.password ) {
+	        errors.push( ".password" );
+	      }
 
-    return Model;
+	      return errors.length > 0 ? errors : false;
+	    }
+	  });
+
+  return Model;
 });
