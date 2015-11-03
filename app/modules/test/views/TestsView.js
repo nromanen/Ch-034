19e0819206fe79189b1a541861a40cf6f3883ea9
Alena Shevchenko <alenka_programmer@mail.ru>
2015-11-03 13:55:25 +0200

    minor fix

--- app/modules/test/views/TestsView.js
+++ app/modules/test/views/TestsView.js
@@ -3,7 +3,7 @@ define(function(require) {
 
     require("jquery-serialize-object");
 
-    var CMS = require("CMS"),            
+    var CMS = require("CMS"),
         TestView = require("./TestView"),
         PaginationView = require("./PaginationView"),
 
@@ -14,17 +14,17 @@ define(function(require) {
         events: {
             "submit"                 : "submitHandler",
             "click .pagination li a" : "submitHandler",
-            "click #test-mode"       : "submitHandler",            
+            "click #test-mode"       : "submitHandler"
         },
         initialize: function(collection, options) {
             this.mode        = options.mode;
             this.toogleMode  = options.toogleMode;
             this.courseId    = options.courseId;
-            this.moduleId    = options.moduleId; 
+            this.moduleId    = options.moduleId;
             this.typeTest    = options.typeTest;
-            this.userAnswers = options.storage;               
+            this.userAnswers = options.storage;
             this.userAnswers.fetch();
-            this.listenTo(this.collection, "reset sync request", this.render);           
+            this.listenTo(this.collection, "reset sync request", this.render);
         },
         serialize: function(){
             return {
@@ -42,7 +42,7 @@ define(function(require) {
                     "nav", new PaginationView({collection: this.collection}, {answers: this.userAnswers})
                 );  
             }
-            this.collection.each(this.renderOne, this);            
+            this.collection.each(this.renderOne, this);
         },
         renderOne: function(model) {
             var answer = "";
@@ -53,16 +53,15 @@ define(function(require) {
         },
         submitHandler: function (e) {
             e.preventDefault();   
-            this.$form = this.$(".tests-form");                         
-            var checkboxEl = [];              
-            var answerForm = this.$form.serializeObject();                     
+            this.$form = this.$(".tests-form");
+            var answerForm = this.$form.serializeObject();
             _.each(answerForm, function(value, key, list){
                 var num = parseInt(key.substring(6, key.length));
                 if (_.isArray(value)) {
-                    value = _.map(value, function(answer){ return answer.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&#34;'); });
+                    value = _.map(value, function(answer){ return answer.replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&#34;"); });
                 }
                 else {
-                    value = value.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&#34;');
+                    value = value.replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&#34;");
                 }
                 if (!_.isEmpty(value)) {
                     this.userAnswers.create({
@@ -74,15 +73,15 @@ define(function(require) {
                 }
                 else if(!_.isUndefined(this.userAnswers.get(num))) {
                     this.userAnswers.get(num).destroy();
-                }          
-            }, this); 
-            _.each(this.$form.find("[type='checkbox']"), function(obj){ checkboxEl.push(obj.name); });
+                }
+            }, this);
+            var checkboxEl = _.pluck(this.$form.find("[type='checkbox']"), "name");
             _.each(checkboxEl, function(arr){ 
                 var num = Number(arr.match(/\d+/)[0]);
                 if ((!(_.has(answerForm, arr)) )&&(!_.isUndefined(this.userAnswers.get(num)))) {
                     this.userAnswers.get(num).destroy();
-                };  
-            }, this);                     
+                }  
+            }, this);
         }
     });
 
