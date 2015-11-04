8f38d02d6fb6fcabdd2b4c0b34a243f7e388467a
unknown <abiliatc@chclass09.training.local>
2015-11-04 16:25:28 +0200

    Validation improved

--- app/modules/register/model/registerModel.js
+++ app/modules/register/model/registerModel.js
@@ -16,18 +16,18 @@ define(function(require){
         validate: function(attr, options) {
             var errors = [];
 
-            if (!attr.name) {
+            if (!(this.isName(attr.name))) {
                 errors.push("name");
             }
 
-            if (!attr.surname) {
+            if (!(this.isSurname(attr.surname))) {
                 errors.push("surname");
             }
             if (!(this.isEmail(attr.email))) {
                 errors.push("email");
             }
 
-            if (!attr.pass) {
+            if (!(this.isPass(attr.pass))) {
                 errors.push("pass");
             }
 
@@ -38,10 +38,22 @@ define(function(require){
             return errors.length > 0 ? errors : false;
         },
 
+        isName: function (name) {
+            return name.length > 1;
+        },
+
+        isSurname: function (surname) {
+            return surname.length > 1;
+        },
+
         isEmail: function (email) {
             var emailRegex = /^[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~-]+(\.[a-zA-Z0-9!#$%&'*+\/=?^_`{|}~-]+)*@([a-zA-Z0-9]([a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+([a-zA-Z]{2,4}|museum|travel)$/;
             return emailRegex.test(email);
         },
+
+        isPass: function (pass) {
+            return pass > 5;
+        }
 
         isPassEqual: function (pass, repeatPass) {
             return pass === repeatPass;
