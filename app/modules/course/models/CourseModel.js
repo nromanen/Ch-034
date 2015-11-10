3f615384e989e84662bf9d972c350815a5b808e2
Tetyana Skutar <zironka81@yahoo.com>
2015-11-10 21:16:41 +0200

    added momentjs

--- app/modules/course/models/CourseModel.js
+++ app/modules/course/models/CourseModel.js
@@ -2,6 +2,7 @@ define(function(require){
     "use strict";
 
     var CMS = require("CMS"),
+        moment = require("moment"),
 
     Model = CMS.Model.extend({
         defaults: {
@@ -11,13 +12,24 @@ define(function(require){
             area: null,
             name: null,
             description: null,
-            start: null,
-            end: null,
+            startDate: null,
             duration: null,
+            endDate: null,
             schedule: null,
             minStudents: null,
             image: null,
             modules: []
+        },
+
+        parse: function(data, options) {
+            this.startDate = moment(data.startDate);
+            this.duration = data.duration;
+            this.endDate = this.setEndDate();
+            return data;
+        },
+
+        setEndDate: function() {
+            return moment(this.startDate.add(this.duration * 7, 'days').toDate());
         },
 
         urlRoot: CMS.api+'courses',
