726afc3900026a267e2298019b1e8702022352d9
vmehera123 <v.mehera123@gmail.com>
2015-11-18 18:09:15 +0200

    fixed

--- server/middlewares/cors.js
+++
@@ -1,8 +0,0 @@
-var express = require('express'),
-cors = function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Expose-Headers", "X-Total-Count");
-    next();
-};
-module.exports = cors;
