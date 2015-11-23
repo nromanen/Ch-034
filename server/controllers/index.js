f5843c5727a405385576446bcca5c0ff679e8375
Alena Shevchenko <alenka_programmer@mail.ru>
2015-11-24 01:28:50 +0200

    connect to tests via express and mongoose

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,12 +1,13 @@
 var express = require('express'),
     router = express.Router();
 
-router.use('/courses', require('./courses'));
-router.use('/courses/:courseId/modules', require('./modules'));
-router.use('/areas', require('./areas'));
-router.use('/groups', require('./groups'));
-router.use('/modules', require('./modules'));
-router.use('/vacancies', require('./vacancies'));
-
+    router.use('/courses', require('./courses'));
+    router.use('/courses/:courseId/modules', require('./modules'));
+    router.use('/areas', require('./areas'));
+    router.use('/groups', require('./groups'));
+    router.use('/modules', require('./modules'));
+    router.use('/courses/:courseId/modules/:moduleId/tests', require('./tests'));
+    router.use('/courses/:courseId/modules/:moduleId/questions', require('./questions'));
+    router.use('/vacancies', require('./vacancies'));
 
 module.exports = router;
