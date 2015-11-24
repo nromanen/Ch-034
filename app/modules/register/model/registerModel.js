7141e29e95ea94abb7b7b6ae66bc7beba5ee41cb
unknown <abiliatc@chclass09.training.local>
2015-11-24 19:34:44 +0200

    urlRoot added

--- app/modules/register/model/registerModel.js
+++ app/modules/register/model/registerModel.js
@@ -16,6 +16,7 @@ define(function(require) {
             pass       : null,
             repeatPass : null
         },
+        urlRoot: "http://localhost:8888/api/signup",
         validation: {
             name: {
                 required: true,
