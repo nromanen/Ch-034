84f7a795ab9dbc2d05dbbf615d69e078435cc1bd
Sergiy Diak <buispr@gmail.com>
2015-11-27 02:30:16 +0200

    API: Added initial error hanlder middleware

--- server/controllers/vacancies.js
+++ server/controllers/vacancies.js
@@ -7,18 +7,24 @@ var express = require('express'),
 router.get('/', function(req, res, next) {
     var vacancies = [];
     request(url, function(err, resp, body){
-        if (err) next(err);
+        if (err) {
+            next(err); 
+        } else {
+            $ = cheerio.load(body);
+            links = $('a.card-vacancy-link').slice(0,5);
+            $(links).each(function(i, link){
+                var name = $(link).find('h3').text(),
+                    url = $(link).attr('href');
 
-        $ = cheerio.load(body);
-        links = $('a.card-vacancy-link').slice(0,5);
-        $(links).each(function(i, link){
-            var name = $(link).find('h3').text(),
-                url = $(link).attr('href');
+                vacancies.push({name: name, url: url})
+            });
 
-            vacancies.push({name: name, url: url})
-        });
-
-        return vacancies.length > 0 ? res.json(vacancies) : res.status(204).json({success:false, message: "Can't parse vacancies"});
+            return vacancies.length > 0 
+                    ? res.json(vacancies) 
+                    : res.status(204).json({success:false, message: "Can't parse vacancies"});
+        }
+        
+        
     });
     
 });
