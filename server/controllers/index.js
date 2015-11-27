6fe0e2b1e25379616e0fe7a77cf36c8024e1e54e
Sergiy Diak <buispr@gmail.com>
2015-11-27 10:08:21 +0200

    Fixed merging bugs

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,6 +2,8 @@ var express = require('express'),
     router = express.Router();
 router.use('/courses', require('./courses'));
 router.use('/courses/:courseId/modules', require('./modules'));
+router.use('/courses/:courseId/modules/:moduleId/tests', require('./tests'));
+router.use('/courses/:courseId/modules/:moduleId/questions', require('./questions'));
 router.use('/areas', require('./areas'));
 router.use('/groups', require('./groups'));
 router.use('/vacancies', require('./vacancies'));
