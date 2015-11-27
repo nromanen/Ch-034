c67a03b7eff231652b2774f858bb449207783227
Sergiy Diak <buispr@gmail.com>
2015-11-27 13:52:25 +0200

    CLIENT: FIXED rangy require errors

--- app/config.js
+++ app/config.js
@@ -1,21 +1,22 @@
 require.config({
+    waitSeconds: 0,
     baseUrl: '/',
     paths: {
         'jquery'                  : '../vendor/bower/jquery/dist/jquery.min',
+        'rangy'                   : '../vendor/bower/wysihtml5x/lib/rangy/rangy-core',
         'jquery-serialize-object' : '../vendor/bower/jquery-serialize-object/dist/jquery.serialize-object.min',
         'lodash'                  : '../vendor/bower/lodash/lodash.min',
         'backbone'                : '../vendor/bower/backbone/backbone-min',
         'bootstrap'               : '../vendor/bower/bootstrap/dist/js/bootstrap.min',
         'text'                    : '../vendor/bower/requirejs-text/text',
-        'CMS'                     : './app',
         'backbone.layoutmanager'  : '../vendor/bower/layoutmanager/backbone.layoutmanager',
         'backbone.localStorage'   : '../vendor/bower/backbone.localStorage/backbone.localStorage-min',
         'backbone.validation'     : '../vendor/bower/backbone.validation/dist/backbone-validation-amd-min',
         'moment'                  : '../vendor/bower/moment/moment',
         'uk-locale'               : '../vendor/bower/moment/locale/uk',
-        'rangy'                   : '../vendor/bower/rangy-official/rangy-core',
         'bootstrap.wysihtml5'     : '../vendor/bower/bootstrap3-wysihtml5-bower/dist/amd/bootstrap3-wysihtml5.all',
-        'bootstrap.wysihtml5.ua-UA': '../vendor/bower/bootstrap3-wysihtml5-bower/dist/locales/bootstrap-wysihtml5.ua-UA'
+        'bootstrap.wysihtml5.ua-UA': '../vendor/bower/bootstrap3-wysihtml5-bower/dist/locales/bootstrap-wysihtml5.ua-UA',
+        'CMS'                     : './app'
     },
 
     map: {
@@ -33,6 +34,10 @@ require.config({
         },
         'jquery-serialize-object': {
             deps: ['jquery']
+        },
+        'backbone.localStorage': {
+            deps: ['backbone'],
+            exports: 'Backbone'
         },
         'backboneValidation' : {
             deps: ['backbone'],
