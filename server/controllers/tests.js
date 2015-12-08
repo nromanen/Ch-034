f181ec35798ea77da933a17d660362e93ca552fd
Alena Shevchenko <alenka_programmer@mail.ru>
2015-12-08 13:59:27 +0200

    add full-stack of operations for TESTS in management

--- server/controllers/tests.js
+++ server/controllers/tests.js
@@ -5,7 +5,6 @@ var express = require("express"),
     Test = require("../models/test");
 
 router.get("/", function(req, res) {
-
     if(req.params.courseId && req.params.moduleId){
         Test
             .findOne({"_course": req.params.courseId, "_module": req.params.moduleId})
@@ -16,7 +15,7 @@ router.get("/", function(req, res) {
     }
     else if(req.params.moduleId){
         Test
-            .findOne({"_module": req.params.moduleId})
+            .find({"_module": req.params.moduleId})
             .sort({"num": 1})
             .exec(function(error, test) {
                 if (error) throw error;
@@ -32,6 +31,39 @@ router.get("/", function(req, res) {
         });
     }
 
+});
+
+router.get("/:id", function(req, res) {
+    Test.findById(req.params.id, function(err, test) {
+        if (err) throw err;
+        return res.json(test);
+    });
+});
+
+router.delete("/:id", function(req, res) {
+    Test.findByIdAndRemove({_id: req.params.id}, function(err) {
+        if (err) throw err;
+        return res.json({success: true, message: "Test deleted successfully"});
+    });
+});
+
+router.post("/", function(req, res) {
+    var test = new Test({ 
+        name   : req.body.name,
+        _module: req.params.moduleId
+    });
+
+    test.save(function(err) {
+        if (err) throw err;
+        return res.json({ success: true });
+    });
+});
+
+router.put("/:id", function(req, res) {
+    Test.findByIdAndUpdate({_id: req.params.id}, {name: req.body.name}, function(err, test) {
+      if (err) return handleError(err);
+      return res.json(test);
+    });
 });
 
 module.exports = router;
