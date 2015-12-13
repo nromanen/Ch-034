65c9a5506447cb8c3c124e3920119ded6690c0cd
Alena Shevchenko <alenka_programmer@mail.ru>
2015-12-13 13:56:30 +0200

    start to implement add-form to extended instances

--- app/modules/management/views/managementsView.js
+++ app/modules/management/views/managementsView.js
@@ -28,6 +28,10 @@ define(function(require) {
         afterRender: function(){
             this.$el.find("#managementlist>tr:not(.edit-row):even").addClass('zebra');
             this.$el.find("#" + this.name).addClass("active").find("a").addClass("active");
+            if (this.type == "extended" || this.editView) {
+                this.$el.find("#managementlist").prepend("<tr class='add-row'><td colspan='4'><div class='col-lg-12' id='collapsable-0'></div></td></tr>");
+                this.$collapsable = $("#collapsable-0");
+            }
         },
 
         renderOne: function(el){
@@ -41,12 +45,17 @@ define(function(require) {
         },
 
         addManagement: function () {
-            var managementName = _.escape(this.$el.find("#managementAddInput").val());
-            if(!managementName) return;
-            var management= new Model({name: managementName });
-            management.url = this.collection.url();
-            management.save();
-            this.collection.fetch({reset:true});
+            if(this.type == "extended" ) {
+                this.$el.find(".add-row").fadeToggle("slow");
+            }
+            else {
+                var managementName = _.escape(this.$el.find("#managementAddInput").val());
+                if(!managementName) return;
+                var management= new Model({name: managementName });
+                management.url = this.collection.url();
+                management.save();
+                this.collection.fetch({reset:true});
+            }
         }
      });
 
